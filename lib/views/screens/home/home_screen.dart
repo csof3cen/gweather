70451392f9397dccb7be6b4aa1c269c7a7e75c1b
HamadaHiro <s.moustapha.sy.ndoye@gmail.com>
2022-09-24 20:31:00 +0000

    Added internet_connectivity_checker_package for checking internet access on device

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gweather/config/const.dart';
 import 'package:gweather/models/weather.dart';
 import 'package:gweather/logic/fetch_data.dart';
+import 'package:internet_connectivity_checker/internet_connectivity_checker.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,6 +23,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Stream<bool> stream = InternetConnectivity()
+        .isConnectedToInternet(intervalInMilliseconds: 1000);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -39,7 +43,32 @@ class _HomeScreenState extends State<HomeScreen> {
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 Weather weather = snapshot.data!;
-                return HomeScreenBody(weather: weather);
+                return StreamBuilder(
+                  stream: stream,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData && (snapshot.data as bool) == true) {
+                      return HomeScreenBody(weather: weather);
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else {
+                      return Center(
+                        child: Container(
+                          padding: EdgeInsets.all(kPadding),
+                          decoration: BoxDecoration(
+                            color: const Color(0XFF1f2b47),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          child: Text(
+                            'Veuillez vérifiez l\'accès à internet pour continuer.',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                );
               } else if (snapshot.hasError) {
                 debugPrint(snapshot.error.toString());
                 return Text('${snapshot.error}');
